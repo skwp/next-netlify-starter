8e8082aef255a1afde336ec93da9de75bf1e367f
Yan Pritzker <yan@pritzker.ws>
2024-03-02 18:18:19 -0600

    Fix title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Welcome to the Rise!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
